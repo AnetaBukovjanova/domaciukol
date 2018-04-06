69b79cbba3e3ac36d206879f4e6bf214656989a5
AnetaBukovjanova <anetbukovjanova@gmail.com>
2018-04-06 22:11:21 +0200

    first commit

---
+++ domaciukol/src/main/java/com/github/AnetaBukovjanova/domaciukol/App.java
@@ -0,0 +1,13 @@
+package com.github.AnetaBukovjanova.domaciukol;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
