3bc8790dbf2689166d9d259f61311a90f18ec650
AnetaBukovjanova <anetbukovjanova@gmail.com>
2018-04-08 22:01:17 +0200

    Poslední commit na dev s úpravou importů

--- domaciukol/src/main/java/com/github/AnetaBukovjanova/domaciukol/ui/HomeController.java
+++ domaciukol/src/main/java/com/github/AnetaBukovjanova/domaciukol/ui/HomeController.java
@@ -12,7 +12,6 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-//import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
